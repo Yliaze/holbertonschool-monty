4b228ed45f465794fcda294d88eb0469be33fbe7
Tristan Duchamp <tristanduchamp@hotmail.fr>
2023-05-09 06:03:37 -0700

    Finish push opcode, finish function selector

--- _push.c
+++ _push.c
@@ -2,6 +2,29 @@
 
 /**
  *
-*/
+ */
 
-void push(
+void push(char *line)
+{
+	stack_t new_node = NULL;
+
+	line = atoi(line);
+
+	new_node = malloc(sizeof(stack_t));
+	new_node->n = line;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if(stack->next != NULL)
+	{
+		new->next = stack;
+		stack->prev = new;
+		stack = new;	
+	}
+	else 
+	{
+		free(stack);
+		stack = new;
+	}
+
+}
---
+++ op_select.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *
+ */
+int op_exec(char **line)
+{
+	int counter = 0;
+
+	instruction_t op_select[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		{"\0", NULL}
+	};
+
+	while (op_select != NULL)
+	{
+		if (op_select[counter].opcode == line[0])
+		{
+			op_select[counter].f(line[1]);
+			break;
+		}
+		counter++;
+	}
+	return (0);
+}
